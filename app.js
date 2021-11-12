633bd5c6583b86ac1f0be5a53a52def9df56e540
Yash <yashgupta@shyplite.com>
2021-11-12 16:37:44 +0530

    live server

--- app.js
+++ app.js
@@ -2,74 +2,84 @@ require('dotenv').config()
 const express = require('express');
 const mysql = require('mysql');
 const util = require('util');
-
+var LiveSelect = require('@vlasky/mysql-live-select');
 //create connection 
-const db= mysql.createConnection({
+const liveDb= new LiveSelect({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
+    database: 'nodemysql',
     port: process.env.MYSQL_PORT
-});
-
-
-db.connect(err=>{
+},err=>{
     if(err){
         throw err;
     }
-    console.log("connected");
+    console.log("connected");}
+);
+
+liveDb.select('select * from employee', null,
+LiveMysqlKeySelector.Columns(['id']), [ {
+  table: 'employee',
+  database: 'nodemysql'
+} ]).on('update', function(diff, data){
+  console.log("data ------------------>\n",diff);
 });
 
 const app=express();
-const query = util.promisify(db.query).bind(db);
+// const liveDb.db.query = util.promisify(liveDb.db.liveDb.db.query).bind(db);
 
 //create Database
 
-app.get('/create',async (req,res)=>{
+app.get('/create',(req,res)=>{
     let sql="CREATE DATABASE nodemysql";
-    await query(sql)
+     liveDb.db.query(sql)
     res.send("Database created");
 });
 //create table
-app.get('/table',async (req,res)=>{
+app.get('/table', (req,res)=>{
     let sql = "USE nodemysql";
-    await query(sql);
+     liveDb.db.query(sql);
     sql= "Create TABLE employee(id int AUTO_INCREMENT,ename VARCHAR(255),designation VARCHAR(255),PRIMARY KEY(id))"
-    await query(sql)
+     liveDb.db.query(sql)
     res.send("Table Created");
 });
 //insert data
-app.get('/employee1',async (req,res)=>{
+app.get('/employee1', (req,res)=>{
     let sql = "USE nodemysql";
-    await query(sql);
+     liveDb.db.query(sql);
 
-    let post = {ename:'sudhanshu',designation:"sd"};
+    let post = {ename:'bhuvnesh',designation:"backend developer"};
     sql = "INSERT INTO employee SET ?";
-    await query(sql,post);
+    liveDb.db.query(sql,post);
+     
     res.send("employee Created");
 })
 //get data
-app.get('/getemployee',async (req,res)=>{
-    let sql = "USE nodemysql";
-    await query(sql);
-    sql = "SELECT * FROM employee";
-    var result = await query(sql);
-    console.log(result);
-    res.send("fetched");
+app.get('/getemployee', (req,res)=>{
+    // let sql = "USE nodemysql";
+    // liveDb.db.query(sql);
+    let sql = "SELECT * FROM employee";
+    liveDb.db.query(sql,(err,result)=>{
+        if (err) throw err;
+        console.log("table->>>>>>>>>>>>>>>>>\n",result);
+        res.send("fetched");
+    });
+    
 })
 //update table
-app.get('/update',async (req,res)=>{
-    let sql = "USE nodemysql";
-    await query(sql);
-    sql = "UPDATE employee SET ename ='xxx' WHERE id='1'";
-    await query(sql)
+app.get('/update', (req,res)=>{
+    // let sql = "USE nodemysql";
+    //  liveDb.db.query(sql);
+    let sql = "UPDATE employee SET ename ='bhuvnesh patnaik' WHERE id='44'";
+     liveDb.db.query(sql)
     res.send("Table updated");
 });
 //delete entry
-app.get('/delete',async (req,res)=>{
-    let sql = "USE nodemysql";
-    await query(sql);
-    sql = "DELETE FROM employee WHERE id='1'";
-    await query(sql)
+app.get('/delete', (req,res)=>{
+    // let sql = "USE nodemysql";
+    //  liveDb.db.query(sql);
+    let sql = "DELETE FROM employee WHERE id='55'";
+     liveDb.db.query(sql)
     res.send("deleted");
 });
 app.listen('3030',()=>{
